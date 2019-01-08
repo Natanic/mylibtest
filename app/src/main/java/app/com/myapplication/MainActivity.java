6324d1489c328e1e9715216c636d2fddb9d586b9
zhaolulu <zll@zhongmakj.com>
2019-01-08 11:14:54 +0800

    上传到maven库：本地库 远程仓库

--- app/src/main/java/app/com/myapplication/MainActivity.java
+++ app/src/main/java/app/com/myapplication/MainActivity.java
@@ -3,11 +3,16 @@ package app.com.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.ver.update.VerUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VerUtil verUtil = new VerUtil(this);
+        verUtil.verCheck("25","fc_duyinghaodai_android");
     }
 }
